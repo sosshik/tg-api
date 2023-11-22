e32bcbc95d7ee20244df0cf596d4398fd4879db3
sosshik <caxalexal@gmail.com>
2023-11-22 19:37:43 +0200

    main functionality

---
+++ api.go
@@ -0,0 +1,118 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type HTTPClientInterface interface {
+	PostForm(url string, data url.Values) (*http.Response, error)
+}
+
+type Api struct {
+	TelegramAPI string
+	HTTPClient  HTTPClientInterface
+	callback    map[string]func(*Api, Update)
+}
+
+func (a *Api) AddCallback(f func(*Api, Update), key string) {
+	if a.callback == nil {
+		a.callback = make(map[string]func(*Api, Update))
+	}
+	a.callback[key] = f
+	log.Warnf("callback %s was set", key)
+}
+
+func ParseTelegramRequest(r *http.Request) (Update, error) {
+
+	var update Update
+
+	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
+		return Update{}, fmt.Errorf("could not decode incoming update %w", err)
+	}
+
+	return update, nil
+}
+
+func (a *Api) SendTextToTelegramChat(chatId int, text string) (string, error) {
+
+	response, err := a.HTTPClient.PostForm(a.TelegramAPI, url.Values{"chat_id": {strconv.Itoa(chatId)}, "text": {text}})
+	if err != nil {
+		return "", fmt.Errorf("error when posting text \"%s\" to the chat %d: %w", text, chatId, err)
+	}
+
+	switch code := response.StatusCode; {
+	case code >= 400 && code < 500:
+		return "", fmt.Errorf("bad request: %v", response.Body)
+	case code >= 500 && code < 600:
+		return "", fmt.Errorf("internal server error")
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", fmt.Errorf("error in parsing telegram response %v, from chat id %d: %w", response, chatId, err)
+	}
+	defer response.Body.Close()
+
+	return string(body), nil
+}
+
+func (a *Api) SendMessage(message OutgoingMessage) error {
+	body, err := json.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("error encoding message: %w", err)
+	}
+
+	_, err = http.Post(a.TelegramAPI, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return fmt.Errorf("error sending message: %w", err)
+	}
+
+	return nil
+}
+
+func (a *Api) HandleTelegramWebHook(w http.ResponseWriter, r *http.Request) {
+
+	log.Infof("parsing telegram request %v", r)
+
+	update, err := ParseTelegramRequest(r)
+	if err != nil {
+		log.Warnf("error while parsing update: %v\n", err)
+		return
+	}
+
+	if f, ok := a.callback[update.Message.Text]; ok {
+		f(a, update)
+	} else if update.Message.Location.Latitude != 0 && update.Message.Location.Longitude != 0 {
+		a.callback["/location"](a, update)
+	} else {
+		a.SendMessageWithLog("*Unknown command*", update)
+	}
+
+}
+
+func (a *Api) SendMessageWithLog(text string, update Update) {
+
+	message := OutgoingMessage{
+		ChatId:    update.Message.Chat.Id,
+		Text:      text,
+		ParseMode: "Markdown",
+	}
+
+	log.Infof("Sending \" %s\" message to chat_id: %d", text, update.Message.Chat.Id)
+
+	err := a.SendMessage(message)
+	if err != nil {
+		log.Warnf("got error %s while sending start message to telegram, chat id is %d", err, update.Message.Chat.Id)
+		return
+	} else {
+		log.Infof("message \" %s\" successfuly distributed to chat id %d", text, update.Message.Chat.Id)
+	}
+}
---
+++ api_test.go
@@ -0,0 +1,77 @@
+package api
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"testing"
+
+	"git.foxminded.ua/foxstudent106264/tgapi/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSendTextToTelegramChat(t *testing.T) {
+	token := "234khnc82347in29289324nkdsc"
+
+	mockClient := &mocks.HTTPClientInterface{}
+	app := &Api{
+		TelegramAPI: "https://api.telegram.org/bot" + token + "/sendMessage",
+		HTTPClient:  mockClient,
+	}
+
+	tests := []struct {
+		name           string
+		expectedURL    string
+		expectedChatID int
+		expectedText   string
+		mockResponse   *http.Response
+		mockError      error
+		expectedResult string
+		expectedError  bool
+	}{
+		{
+			"Positive Test",
+			app.TelegramAPI,
+			123,
+			"your_message",
+			&http.Response{Body: io.NopCloser(strings.NewReader("message your_message successfuly distributed to chat id 123")), StatusCode: http.StatusOK},
+			nil,
+			"message your_message successfuly distributed to chat id 123",
+			false,
+		},
+		{
+			"Negative Test",
+			app.TelegramAPI,
+			123,
+			"your_message",
+			nil,
+			errors.New("err"),
+			"",
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			mockClient.On("PostForm", test.expectedURL, url.Values{
+				"chat_id": {strconv.Itoa(test.expectedChatID)},
+				"text":    {test.expectedText},
+			}).Return(test.mockResponse, test.mockError)
+
+			response, err := app.SendTextToTelegramChat(test.expectedChatID, test.expectedText)
+
+			if test.expectedError {
+				assert.Empty(t, response, "Response should be empty on error")
+			} else {
+				assert.NoError(t, err, "Unexpected error")
+				assert.Equal(t, test.expectedResult, response, "Unexpected response")
+			}
+
+			mockClient.AssertExpectations(t)
+		})
+	}
+}
